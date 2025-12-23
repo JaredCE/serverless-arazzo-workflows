c087e8239deaed843a3a2cbc5a606529573786d6
Jared Evans <jarede@gmail.com>
2025-12-23 11:08:51 +0000

    improve logging and writing the file

--- src/ArazzoPlugin.js
+++ src/ArazzoPlugin.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const chalk = require("chalk");
+const yaml = require("js-yaml");
+
 const fs = require('fs/promises')
+
+const ArazzoGenerator = require('./ArazzoGenerator');
+const Logger = require("./Logger");
 
 const serverlessSchema = require('../schema/serverlessSchema.json')
 
 class ArazzoPlugin {
-    constructor(serverless) {
+    constructor(serverless, options, {log={}} = {}) {
         this.serverless = serverless;
+        this.logOutput = log;
+
+        this.logger = new Logger(this.serverless, this.logOutput);
 
         this.commands = {
             arazzo: {
@@ -40,25 +49,57 @@ class ArazzoPlugin {
             "arazzo:generate:serverless": this.generate.bind(this),
         };
 
-        this.serverless.configSchemaHandler.defineCustomProperties(serverlessSchema);
+        // this.serverless.configSchemaHandler.defineCustomProperties(serverlessSchema);
     }
 
     async generate() {
+        this.logger.notice(
+            chalk.bold.underline("Arazzo v1 Specification Generation")
+        );
+
         this.processCLIInput();
+
+        await this.arazzoGeneration();
     }
 
     async arazzoGeneration() {
+        const generator = new ArazzoGenerator(
+            this.serverless.service.custom.arazzo,
+            {
+                arazzo: this.config.arazzoVersion,
+                sls: this.serverless,
+                sourceFile: this.config.source
+            }
+        );
+
+        this.logger.notice(`Generating Arazzo Specification`);
+        generator.parse();
+
+        this.logger.success("Arazzo Specification Successfully Generated");
+
+        this.arazzoSpecification = generator.arazzo;
+
         await this.writeArazzoFile();
     }
 
     async writeArazzoFile() {
-        await fs.writeFile(this.config.file, '');
+        let output;
+
+        if (this.config.format === 'yml') {
+            output = yaml.dump(this.arazzoSpecification)
+        } else {
+            output = JSON.stringify(this.arazzoSpecification, null)
+        }
+
+        await fs.writeFile(this.config.file, output);
+
+        this.logger.success("Arazzo Specification Successfully Written");
     }
 
     processCLIInput() {
         const config = {
             format: "json",
-            file: "arazzo",
+            file: "arazzo.json",
             arazzoVersion: "1.0.1",
             validationWarn: false,
             source: 'openapi.json'
@@ -75,9 +116,7 @@ class ArazzoPlugin {
         }
 
         if (this.serverless.processedInput.options.output) {
-            config.file = `${this.serverless.processedInput.options.output}.${config.format}`;
-        } else {
-            config.file = `arazzo.${config.format}`;
+            config.file = `${this.serverless.processedInput.options.output}`;
         }
 
         if (this.serverless.processedInput.options.source) {
@@ -85,6 +124,16 @@ class ArazzoPlugin {
         }
 
         this.config = config;
+
+        this.logger.notice(
+            `
+${chalk.bold.green("[OPTIONS]")}
+    Arazzo Version: "${chalk.bold.green(String(this.config.arazzoVersion))}"
+    Format: "${chalk.bold.green(this.config.format)}"
+    Output File: "${chalk.bold.green(this.config.file)}"
+    Source File: "${chalk.bold.green(this.config.source)}"
+            `
+        )
     }
 }
 
