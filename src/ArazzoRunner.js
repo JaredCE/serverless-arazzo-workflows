619ba597f416115774a901ad2185d85c6cb623c7
Jared Evans <jarede@gmail.com>
2025-12-24 15:31:42 +0000

    Pick out our source descriptions

--- src/ArazzoRunner.js
+++ src/ArazzoRunner.js
@@ -1,10 +1,41 @@
 'use strict';
 
-const fs = require('node:fs/promises')
+const Pick = require('stream-json/filters/Pick');
+const {streamValues} = require('stream-json/streamers/StreamValues');
+const {chain} = require('stream-chain');
+
+const fs = require('node:fs');
 
 class ArazzoRunner {
-    constructor(pathToArazzoSpecification = './arazzo.json') {
+    constructor(pathToArazzoSpecification = './arazzo.json', options) {
         this.pathToArazzoSpecification = pathToArazzoSpecification;
+
+        this.logger = options.logger;
+
+    }
+
+    async runArazzoWorkflows() {
+        await this.getSourceDescriptions();
+    }
+
+    async getSourceDescriptions() {
+        const pipeline = this.JSONPicker('sourceDescriptions', this.pathToArazzoSpecification);
+
+        let sourceDescriptions = [];
+        for await (const { value } of pipeline) {
+            sourceDescriptions = value.flat();
+        }
+
+        this.sourceDescriptions = sourceDescriptions;
+    }
+
+    JSONPicker(key, file) {
+        const pipeline = chain([
+            fs.createReadStream(file),
+            Pick.withParser({filter: key}),
+            streamValues()
+        ]);
+        return pipeline
     }
 }
 
