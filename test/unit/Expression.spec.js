7e495f9e2f849055bd355d4c82cc0dc328cd1732
Jared Evans <jarede@gmail.com>
2026-01-08 19:01:08 +0000

    update tests

--- test/unit/Expression.spec.js
+++ test/unit/Expression.spec.js
@@ -89,6 +89,17 @@ describe(`Expression`, function () {
                 expect(expected).to.be.eql({ name: 'john' });
             });
 
+            xit(`should resolve a dotted expression for a steps output`, function() {
+                const expression = new Expression();
+
+                // expression.addToContext('steps.createAUser.outputs.user', { name: 'john' });
+                expression.addToContext('steps', {createAUser: {outputs: {user: {name: 'john'}}}});
+
+                const expected = expression.resolveExpression('$steps.createAUser.outputs.user');
+
+                expect(expected).to.be.eql({ name: 'john' });
+            });
+
             it(`should resolve a dotted expression with a json pointer for a response body`, function() {
                 const expression = new Expression();
 
@@ -113,6 +124,22 @@ describe(`Expression`, function () {
                 const expected = expression.resolveExpression('$response.header.x-rate-limit');
 
                 expect(expected).to.be.eql({ 'x-rate-limit': '500' });
+            });
+
+            it(`should resolve a dotted expression for a specific request QueryParams`, function() {
+                const expression = new Expression();
+                const queryParams = new URLSearchParams()
+                queryParams.append('username', 'bob');
+                const contextQueryParams = {}
+                for (const [queryParam, value] of queryParams.entries()) {
+                    Object.assign(contextQueryParams, {[queryParam]: value});
+                }
+
+                expression.addToContext('request.query', contextQueryParams);
+
+                const expected = expression.resolveExpression('$request.query.username');
+
+                expect(expected).to.be.eql({ 'username': 'bob' });
             });
         });
 
