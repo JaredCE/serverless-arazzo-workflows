255ece25ea396df50d3ae508c504af5088a789b6
Jared Evans <jarede@gmail.com>
2026-01-09 18:46:14 +0000

    create a setStepFailureActions test

--- test/unit/Rules.spec.js
+++ test/unit/Rules.spec.js
@@ -9,6 +9,8 @@ describe(`Rules`, function () {
     const expected = new Rules();
 
     expect(expected).to.be.an.instanceOf(Rules);
+    expect(rules.rules).to.be.an("array");
+    expect(rules.rules).to.have.lengthOf(0);
   });
 
   describe(`setWorkflowFailures`, function () {
@@ -28,6 +30,43 @@ describe(`Rules`, function () {
       expect(rules).to.have.property("workflowFailures");
       expect(rules.workflowFailures).to.be.an("array");
       expect(rules.workflowFailures).to.have.lengthOf(1);
+      expect(rules.rules).to.be.an("array");
+      expect(rules.rules).to.have.lengthOf(1);
+    });
+  });
+
+  describe(`setStepFailureActions`, function () {
+    it(`should take a list of Step Failure Actions and combine with Workflow Failure Actions`, function () {
+      const rules = new Rules();
+
+      const workflowOnFailures = [
+        {
+          name: "404Failure",
+          type: "end",
+          criteria: [{ condition: "$statusCode == 404" }],
+        },
+      ];
+
+      rules.setWorkflowFailures(workflowOnFailures);
+
+      const stepOnFailures = [
+        {
+          name: "404Failure",
+          type: "end",
+          criteria: [{ condition: "$statusCode == 404" }],
+        },
+      ];
+
+      rules.setStepFailures(workflowOnFailures);
+
+      expect(rules).to.have.property("workflowFailures");
+      expect(rules).to.have.property("stepFailures");
+      expect(rules.workflowFailures).to.be.an("array");
+      expect(rules.workflowFailures).to.have.lengthOf(1);
+      expect(rules.stepFailures).to.be.an("array");
+      expect(rules.stepFailures).to.have.lengthOf(1);
+      expect(rules.rules).to.be.an("array");
+      expect(rules.rules).to.have.lengthOf(2);
     });
   });
 });
