17bb1ddd742d2b06a7139cd41b683411957a60fb
Jared Evans <jarede@gmail.com>
2026-01-09 18:36:55 +0000

    create a test for Workflow failures

--- test/unit/Rules.spec.js
+++ test/unit/Rules.spec.js
@@ -10,4 +10,24 @@ describe(`Rules`, function () {
 
     expect(expected).to.be.an.instanceOf(Rules);
   });
+
+  describe(`setWorkflowFailures`, function () {
+    it(`should take a list of Workflow Failure Actions`, function () {
+      const rules = new Rules();
+
+      const onFailures = [
+        {
+          name: "404Failure",
+          type: "end",
+          criteria: [{ condition: "$statusCode == 404" }],
+        },
+      ];
+
+      rules.setWorkflowFailures(onFailures);
+
+      expect(rules).to.have.property("workflowFailures");
+      expect(rules.workflowFailures).to.be.an("array");
+      expect(rules.workflowFailures).to.have.lengthOf(1);
+    });
+  });
 });
