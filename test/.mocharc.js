55bff755bfa8fd7c3b6187b8dada34d247252af2
Jared Evans <jarede@gmail.com>
2026-01-10 16:34:00 +0000

    prettifies

--- test/.mocharc.js
+++ test/.mocharc.js
@@ -1,9 +1,9 @@
-'use strict'
+"use strict";
 
 module.exports = {
-    recursive: true,
-    reporter: 'spec',
-    spec: 'test/unit/*.spec.js',
-    watch: false,
-    'watch-files': ['src/**/*.js', 'test/**/*.spec.js'],
-}
+  recursive: true,
+  reporter: "spec",
+  spec: "test/unit/*.spec.js",
+  watch: false,
+  "watch-files": ["src/**/*.js", "test/**/*.spec.js"],
+};
